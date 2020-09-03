ff24c62da01ecb554005424e855313a836f54b55
Rodrigo Nunes <69399583+rodrigonuness@users.noreply.github.com>
2020-09-03 09:06:09 -0300

    Create root.py

---
+++ root.py
@@ -0,0 +1,9 @@
+cont = "c"
+while (cont == "c"):
+        def square_root(x, y):
+                print(x ** (1 / y))
+
+        rooting = int(input("input rooting: "))
+        index = int(input("input index: "))
+        square_root(rooting, index)
+        cont = input("for continue press c: ")
